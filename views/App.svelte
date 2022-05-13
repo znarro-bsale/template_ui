14138b7848ef8ab0efb922f4f2a98261bedfbc5b
Zamir Narro <znarro@bsale.com.pe>
2022-05-13 15:29:03 -0500

    Add preventDefault

--- views/App.svelte
+++ views/App.svelte
@@ -19,7 +19,7 @@
 	<h1>Hello {name}</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 
-	<form action="">
+	<form action="" on:submit|preventDefault={() => {}}>
 		<Smui />
 	</form>
 </main>
